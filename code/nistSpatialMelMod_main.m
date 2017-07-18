fd554f5a158b0c36904ee27a1a17e2ff74d31934
gkaguirre <gkaguirre@me.com>
2017-07-18 13:01:22 -0400

    Modulation primaries returns this, plus items needed for SPD generation
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/nistSpatialMelMod_main.m
+++ code/nistSpatialMelMod_main.m
@@ -3,4 +3,60 @@ codeBaseDir = tbLocateProject('nistSpatialMelMod','verbose',false);
 
 calibrationFileName = fullfile(codeBaseDir, 'demoOneLightCalFile', 'OneLightDemoCal.mat');
 
-[ backgroundPrimary, modulationPrimary ] = nistSpatialMelMod_makeModulationSPD( calibrationFileName );
+[ backgroundPrimary, modulationPrimary, B_primary, ambientSpd, T_receptors ] = nistSpatialMelMod_makeModulationPrimaries( calibrationFileName, 'desiredContrast',0.3, 'verbose', true );
+
+figure
+plot(backgroundPrimary,'-k');
+hold on
+plot(modulationPrimary,'-r');
+
+[ backgroundPrimary, modulationPrimary, B_primary, ambientSpd, T_receptors ] = nistSpatialMelMod_makeModulationPrimaries( calibrationFileName, 'desiredContrast',0.15, 'verbose', true );
+
+plot(modulationPrimary,'-.b');
+
+
+backgroundReceptors = T_receptors*(B_primary*backgroundPrimary + ambientSpd);
+modulationReceptors = T_receptors*B_primary*(modulationPrimary - backgroundPrimary);
+contrastReceptors = modulationReceptors ./ backgroundReceptors;
+
+
+
+
+% %% Plots
+% plotDir = fullfile(codeBaseDir,'outputPlots');
+% if ~isdir(plotDir)
+%     mkdir(plotDir);
+% end
+% curDir = pwd;
+% cd(plotDir);
+% 
+% % Photoreceptor sensitivities
+% theFig1 = figure; clf; hold on
+% plot(SToWls(S),T_receptors,'LineWidth',2);
+% xlabel('Wavelength (nm)')
+% ylabel('Sensitivity');
+% title('Normalized photoreceptor sensitivities');
+% %saveas(theFig1,sprintf('%s_%s_%s_Sensitivities.pdf',whichModel,whichPrimaries,whichDirectionNumber),'pdf');
+% 
+% % Modulation spectra
+% theFig2 = figure; hold on
+% plot(SToWls(S),B_primary*modulationPrimary,'r','LineWidth',2);
+% plot(SToWls(S),B_primary*backgroundPrimary,'k','LineWidth',2);
+% title('Modulation spectra');
+% xlim([380 780]);
+% xlabel('Wavelength');
+% ylabel('Power');
+% pbaspect([1 1 1]);
+% %saveas(theFig2,sprintf('%s_%s_%s_Modulation.pdf',whichModel,whichPrimaries,whichDirectionNumber),'pdf');
+% 
+% % Primaries
+% theFig3 = figure; hold on
+% plot(modulationPrimary,'r','LineWidth',2);
+% plot(backgroundPrimary,'k','LineWidth',2);
+% title('Primary settings');
+% xlim([0 length(backgroundPrimary)]);
+% ylim([0 1]);
+% xlabel('Primary Number (nominal)');
+% ylabel('Setting');
+% %saveas(theFig3,sprintf('%s_%s_%s_Primaries.pdf',whichModel,whichPrimaries,whichDirectionNumber),'pdf');
+
--- code/nistSpatialMelMod_makeModulationPrimaries.m
+++ code/nistSpatialMelMod_makeModulationPrimaries.m
@@ -1,4 +1,4 @@
-function [ backgroundPrimary, modulationPrimary ] = nistSpatialMelMod_makeModulationSPD( calibrationFileName, varargin )
+function [ backgroundPrimary, modulationPrimary, B_primary, ambientSpd, T_receptors ] = nistSpatialMelMod_makeModulationPrimaries( calibrationFileName, varargin )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -13,7 +13,7 @@ function [ backgroundPrimary, modulationPrimary ] = nistSpatialMelMod_makeModula
 %	whichReceptorsToIgnore - do not care if we silence or stimulate these
 %       (All remaining photoreceptors are silenced)
 %   desiredContrast - vector of contrast to be sought for each of the 
-%       targeted receptor classes. Set entries to Inf to maximize contrast.
+%       targeted receptor classes. Set to empty to maximize contrast.
 %        
 % OPTIONAL - device parameters:
 %   primaryHeadRoom -  This parameter enforces a constraint that we don't
@@ -63,7 +63,7 @@ p.addParameter('photoreceptorClasses', ...
     'SConeTabulatedAbsorbancePenumbral'},@iscell);
 p.addParameter('whichReceptorsToTarget',[4],@(x)(isempty(x) | isnumeric(x)));
 p.addParameter('whichReceptorsToIgnore',[5 6 7 8],@(x)(isempty(x) | isnumeric(x)));
-p.addParameter('desiredContrast',[Inf],@isnumeric);
+p.addParameter('desiredContrast',[0.4],@(x)(isempty(x) | isnumeric(x)));
 
 % Optional - observer and physiologic parameters
 p.addParameter('whichModel','HumanPhotopigments',@ischar);
@@ -73,6 +73,7 @@ p.addParameter('pupilDiameterMm',4.7,@isnumeric);
 p.addParameter('vesselOxyFraction',0.85,@isnumeric);
 p.addParameter('vesselOverallThicknessUm',5,@isnumeric);
 p.addParameter('correctBleaching',true,@islogical);
+
 
 %% Parse and check the parameters
 p.parse(calibrationFileName, varargin{:});
@@ -91,8 +92,6 @@ backgroundPrimary = 0.5*ones(size(B_primary,2),1);
 
 % Do not pin any of the primaries
 whichPrimariesToPin = [];
-
-
 
 
 
@@ -124,22 +123,22 @@ if p.Results.correctBleaching
         GetConeFractionBleachedFromSpectrum(S, B_primary*backgroundPrimary + ambientSpd, p.Results.fieldSizeDegrees, p.Results.observerAgeInYears, p.Results.pupilDiameterMm, [], p.Results.verbose);
     
     % Assign the fraction bleached for each photoreceptor class.
-    for p = 1:length(p.Results.photoreceptorClasses)
-        switch p.Results.photoreceptorClasses{p}
+    for ii = 1:length(p.Results.photoreceptorClasses)
+        switch p.Results.photoreceptorClasses{ii}
             case 'LCone'
-                fractionBleached(p) = fractionBleachedFromIsom(1);
+                fractionBleached(ii) = fractionBleachedFromIsom(1);
             case 'MCone'
-                fractionBleached(p) = fractionBleachedFromIsom(2);
+                fractionBleached(ii) = fractionBleachedFromIsom(2);
             case 'SCone'
-                fractionBleached(p) = fractionBleachedFromIsom(3);
+                fractionBleached(ii) = fractionBleachedFromIsom(3);
             case 'LConeHemo'
-                fractionBleached(p) = fractionBleachedFromIsomHemo(1);
+                fractionBleached(ii) = fractionBleachedFromIsomHemo(1);
             case 'MConeHemo'
-                fractionBleached(p) = fractionBleachedFromIsomHemo(2);
+                fractionBleached(ii) = fractionBleachedFromIsomHemo(2);
             case 'SConeHemo'
-                fractionBleached(p) = fractionBleachedFromIsomHemo(3);
+                fractionBleached(ii) = fractionBleachedFromIsomHemo(3);
             otherwise
-                fractionBleached(p) = 0;
+                fractionBleached(ii) = 0;
         end
     end
 else
@@ -155,7 +154,7 @@ end
 % The last two arguments are the oxygenation fraction and the
 % vessel thickness. We set them to be empty here, prompting the
 % user to enter these values later.
-T_receptors = GetHumanPhotoreceptorSS(S, p.Results.photoreceptorClasses, p.Results.fieldSizeDegrees, p.Results.observerAgeInYears, p.Results.pupilDiameterMm, [], p.Results.fractionBleached, [], []);
+T_receptors = GetHumanPhotoreceptorSS(S, p.Results.photoreceptorClasses, p.Results.fieldSizeDegrees, p.Results.observerAgeInYears, p.Results.pupilDiameterMm, [], fractionBleached, [], []);
 
 
 % User chooses whether to maximize contrast in targeted receptor classes or
@@ -168,19 +167,19 @@ T_receptors = GetHumanPhotoreceptorSS(S, p.Results.photoreceptorClasses, p.Resul
 
 % Nice message for user
 if p.Results.verbose
-fprintf('\nGenerating stimuli which isolate receptor classes');
-for i = 1:length(whichReceptorsToTarget)
-    fprintf('\n  - %s', photoreceptorClasses{whichReceptorsToTarget(i)});
-end
-fprintf('\nGenerating stimuli which ignore receptor classes');
-if (~length(whichReceptorsToIgnore) == 0)
-    for i = 1:length(whichReceptorsToIgnore)
-        fprintf('\n  - %s', photoreceptorClasses{whichReceptorsToIgnore(i)});
+    fprintf('\nGenerating stimuli which isolate receptor classes');
+    for i = 1:length(p.Results.whichReceptorsToTarget)
+        fprintf('\n  - %s', p.Results.photoreceptorClasses{p.Results.whichReceptorsToTarget(i)});
     end
-else
-    fprintf('\n  - None');
-end
-fprintf('\nThe remaining classes will be silenced\n');
+    fprintf('\nGenerating stimuli which ignore receptor classes');
+    if (~length(p.Results.whichReceptorsToIgnore) == 0)
+        for i = 1:length(p.Results.whichReceptorsToIgnore)
+            fprintf('\n  - %s', p.Results.photoreceptorClasses{p.Results.whichReceptorsToIgnore(i)});
+        end
+    else
+        fprintf('\n  - None');
+    end
+    fprintf('\nThe remaining classes will be silenced\n');
 end
 
 %% Call the optimization routine.
@@ -200,49 +199,10 @@ modulationReceptors = T_receptors*B_primary*(modulationPrimary - backgroundPrima
 contrastReceptors = modulationReceptors ./ backgroundReceptors;
 
 if p.Results.verbose
-for j = 1:size(T_receptors,1)
-    fprintf('\t%s: contrast = %0.4f\n',photoreceptorClasses{j},contrastReceptors(j));
+    for j = 1:size(T_receptors,1)
+        fprintf('\t%s: contrast = %0.4f\n',p.Results.photoreceptorClasses{j},contrastReceptors(j));
+    end
 end
-end
-
-% %% Plots
-% plotDir = fullfile(codeBaseDir,'outputPlots');
-% if ~isdir(plotDir)
-%     mkdir(plotDir);
-% end
-% curDir = pwd;
-% cd(plotDir);
-
-% Photoreceptor sensitivities
-theFig1 = figure; clf; hold on
-plot(SToWls(S),T_receptors,'LineWidth',2);
-xlabel('Wavelength (nm)')
-ylabel('Sensitivity');
-title('Normalized photoreceptor sensitivities');
-%saveas(theFig1,sprintf('%s_%s_%s_Sensitivities.pdf',whichModel,whichPrimaries,whichDirectionNumber),'pdf');
-
-% Modulation spectra
-theFig2 = figure; hold on
-plot(SToWls(S),B_primary*modulationPrimary,'r','LineWidth',2);
-plot(SToWls(S),B_primary*backgroundPrimary,'k','LineWidth',2);
-title('Modulation spectra');
-xlim([380 780]);
-xlabel('Wavelength');
-ylabel('Power');
-pbaspect([1 1 1]);
-%saveas(theFig2,sprintf('%s_%s_%s_Modulation.pdf',whichModel,whichPrimaries,whichDirectionNumber),'pdf');
-
-% Primaries
-theFig3 = figure; hold on
-plot(modulationPrimary,'r','LineWidth',2);
-plot(backgroundPrimary,'k','LineWidth',2);
-title('Primary settings');
-xlim([0 length(backgroundPrimary)]);
-ylim([0 1]);
-xlabel('Primary Number (nominal)');
-ylabel('Setting');
-%saveas(theFig3,sprintf('%s_%s_%s_Primaries.pdf',whichModel,whichPrimaries,whichDirectionNumber),'pdf');
-
 
 
 end
